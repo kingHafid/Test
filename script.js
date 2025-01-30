6e7ab92b0297ac493625c0650a40f64d677c8666
kingHafid <hafidbouawinat768@gmail.com>
2025-01-30 14:48:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+        <blockquote>"وفرت أكثر من 200 دولار بفضل هذه العروض!" - محمد.</blockquote>
+        <p class="terms">العرض ساري حتى نفاد الكمية.</p>
+    </div >
+        <script src="script.js"></script>
+</body >
+</html >
